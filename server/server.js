21d97ffa93316b3d2dd9e7af7d42a0ae709ca42e
Zachary Dowd <zacharyd2796@zohomail.com>
2021-10-15 08:07:39 -0600

    config work

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-
+const mongoose = require("mongoose");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
@@ -31,6 +31,7 @@ mongoose.connect(
     useFindAndModify: false,
   }
 );
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
